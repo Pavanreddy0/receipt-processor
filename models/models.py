378c32789a169b69381f226bc313cd99f0681f29
pavanreddy <pavanpanuganti0@gmail.com>
2024-06-09 20:16:49 -0400

    Added pydantic models for proper field validation

--- models/models.py
+++ models/models.py
@@ -1,31 +1,50 @@
 import json
 from datetime import datetime
-from typing import List
+from typing import List, Optional
+
+from pydantic import BaseModel, Field, validator
 
 
-class Item:
-    description: str
-    price: float
+class Item(BaseModel):
+    description: str = Field(default="", validation_alias='shortDescription', pattern=r'^[\\w\\s\\-]+$')
+    price: float = Field(default=0.00, pattern="^\\d+\\.\\d{2}$")
 
-    def __init__(self, shortDescription: str, price: str):
-        self.description = shortDescription.strip()
-        self.price = float(price)
+    @validator('description', pre=True)
+    def validate_description(cls, value):
+        return value.strip()
+
+    @validator('price', pre=True)
+    def validate_price(cls, value):
+        return float(value)
+
+    class Config:
+        allow_population_by_field_name = True
 
 
-class Receipt:
-    id: str
-    retailer: str
+class Receipt(BaseModel):
+    id: Optional[str]
+    retailer: str = Field(default="", pattern="^[\\w\\s\\-&]+$")
     purchaseDate: datetime
     purchaseTime: datetime
     items: List[Item]
-    total: float
-    points: float
+    total: float = Field(default=0.00, pattern="^\\d+\\.\\d{2}$")
+    points: Optional[float]
 
-    def __init__(self, retailer: str, purchaseDate: str, purchaseTime: str, items: List[dict], total: str):
-        self.retailer = retailer
-        self.purchaseDate = datetime.strptime(purchaseDate, "%Y-%m-%d")
-        self.purchaseTime = datetime.strptime(purchaseTime, "%H:%M")
-        self.items = [Item(**item) for item in items]
-        self.total = float(total)
-        self.points = 0
-        self.id = None
+    @validator('purchaseDate', pre=True)
+    def validate_purchase_date(cls, value):
+        return datetime.strptime(value, "%Y-%m-%d")
+
+    @validator('purchaseTime', pre=True)
+    def validate_purchase_time(cls, value):
+        return datetime.strptime(value, "%H:%M")
+
+    @validator('total', pre=True)
+    def validate_total(cls, value):
+        return float(value)
+
+    @validator('items', pre=True)
+    def validate_items(cls, value):
+        return [Item(**item) for item in value]
+
+    class Config:
+        allow_population_by_field_name = True
